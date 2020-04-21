55b8d89adb52f907969bcee1e7326978be526213
php1301 <19520854@gm.uit.edu.vn>
2020-04-22 00:59:08 +0700

    essential nextjs file update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,45 @@
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/prop-types */
-import 'antd/dist/antd.css';
-import '../styles/style.scss';
+import App from 'next/app';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import { ThemeProvider } from 'styled-components';
+import Layout from 'container/Layout/Layout';
+import AuthProvider from 'context/AuthProvider';
+import { SearchProvider } from 'context/SearchProvider';
+import { withData } from 'library/helpers/restriction';
+import theme from '../themes/default.theme';
+import GlobalStyles from '../assets/style/Global.style';
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default class CustomApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    const { query, pathname } = ctx;
+
+    const { user, isLoggedIn } = withData(ctx);
+    return {
+      pageProps, query, pathname, user, isLoggedIn,
+    };
+  }
+
+  render() {
+    const {
+      Component, pageProps, query, user, isLoggedIn,
+    } = this.props;
+
+    return (
+      <AuthProvider>
+        <SearchProvider query={query}>
+          <Layout user={user} isLoggedIn={isLoggedIn}>
+            <ThemeProvider theme={theme}>
+              <>
+                <GlobalStyles />
+                <Component isLoggedIn={isLoggedIn} {...pageProps} />
+              </>
+            </ThemeProvider>
+          </Layout>
+        </SearchProvider>
+      </AuthProvider>
+    );
+  }
 }
