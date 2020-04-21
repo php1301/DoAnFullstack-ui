2f37a33b354dc5da1f7fdadea4e3b125f73e9b1d
php1301 <19520854@gm.uit.edu.vn>
2020-04-22 00:55:59 +0700

    For routing purpose with nextJS router style, more route will be added, check previous commit

---
+++ settings/constants.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+// **************** ROUTE CHO NEXTJS **************************
+
+// **************** ROUTE CONSTANT START **************************
+// General Page Section
+export const HOME_PAGE = '/';
+
+// Listing Single Page Section
+export const LISTING_POSTS_PAGE = '/listing';
+
+// User Profile Page Section
+export const USER_PROFILE_PAGE = '/profile';
+
+// Other Pages
+export const PRICING_PLAN_PAGE = '/pricing-plan';
+// **************** ROUTE CONSTANT END **************************
