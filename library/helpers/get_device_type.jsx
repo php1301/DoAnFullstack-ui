67b7cf1376f9c68177a262371071f0f925cdabb5
php1301 <19520854@gm.uit.edu.vn>
2020-05-01 04:22:59 +0700

    get_device_type added

---
+++ library/helpers/get_device_type.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable import/prefer-default-export */
+import MobileDetect from 'mobile-detect';
+
+export function getDeviceType(req) {
+  let userAgent;
+  let deviceType;
+  if (req) {
+    userAgent = req.headers['user-agent'];
+  } else {
+    userAgent = navigator.userAgent;
+  }
+  const md = new MobileDetect(userAgent);
+  if (md.tablet()) {
+    deviceType = 'tablet';
+  } else if (md.mobile()) {
+    deviceType = 'mobile';
+  } else {
+    deviceType = 'desktop';
+  }
+  return deviceType;
+}
