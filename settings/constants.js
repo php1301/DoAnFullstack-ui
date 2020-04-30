e63c41a0d0577e4a97a870b4b082a39605b7b62d
php1301 <19520854@gm.uit.edu.vn>
2020-05-01 04:17:22 +0700

    Update routing constants

--- settings/constants.js
+++ settings/constants.js
@@ -7,10 +7,13 @@ export const HOME_PAGE = '/';
 
 // Listing Single Page Section
 export const LISTING_POSTS_PAGE = '/listing';
+export const SINGLE_POST_PAGE = '/post';
 
 // User Profile Page Section
 export const USER_PROFILE_PAGE = '/profile';
 
 // Other Pages
 export const PRICING_PLAN_PAGE = '/pricing-plan';
+export const PRIVACY_PAGE = '/privacy';
+
 // **************** ROUTE CONSTANT END **************************
