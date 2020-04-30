1ff8413f7aebc9c3198ed52dfce086c46b281cd3
php1301 <19520854@gm.uit.edu.vn>
2020-05-01 04:13:53 +0700

    _app.js config fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,10 @@
 import App from 'next/app';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { ThemeProvider } from 'styled-components';
-import Layout from '../container/Layout/Layout';
-
 import theme from '../themes/default.theme';
 import GlobalStyles from '../assets/style/Global.style';
+import Layout from '../container/Layout/Layout';
+import '../style.css';
 
 export default class CustomApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -12,19 +12,24 @@ export default class CustomApp extends App {
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
+    const { query, pathname } = ctx;
+
     return {
-      pageProps,
+      pageProps, query, pathname,
     };
   } // getInitialProps deprecrated
 
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
     return (
+      // Render cứng header và footer -
       <Layout>
+        {/* Các đoạn code ở dưới là children bao gồm cả các pages default index.js */}
         <ThemeProvider theme={theme}>
           <>
             <GlobalStyles />
-            <Component />
+            {/* Các pages - data */}
+            <Component {...pageProps} />
           </>
         </ThemeProvider>
       </Layout>
