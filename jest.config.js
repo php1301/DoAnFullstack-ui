c01628e8ab0155fa0631d6c48ac1ce0d4e8e61b8
php1301 <19520854@gm.uit.edu.vn>
2020-05-03 22:40:39 +0700

    Unit Test context added

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,11 @@ module.exports = {
   setupFiles: ['<rootDir>/settings/setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
+    // media assets parse vào mock fileMock
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/pages/__mocks__/fileMock.js',
+    // styling parse vào mock styleMock
     '\\.(css|scss)$': '<rootDir>/pages/__mocks__/styleMock.js',
+    // Config cho absolute path sử dụng regex để jest đọc file
     '^components/(.*)$': '<rootDir>/components/$1',
     '^library/(.*)$': '<rootDir>/library/$1',
     '^settings/(.*)$': '<rootDir>/settings/$1',
