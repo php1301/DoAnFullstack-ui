9b89281c1373bd961f67ab915c1e40d1150dc37c
php1301 <19520854@gm.uit.edu.vn>
2020-05-08 23:18:56 +0700

    url_handlers added

---
+++ library/helpers/url_handler.jsx
@@ -0,0 +1,85 @@
+export function createUrl(urlData) {
+  const keys = Object.keys(urlData);
+  let search = '';
+  keys.forEach((key) => {
+    // Check null key
+    if (
+      urlData[key]
+      && urlData[key] !== null
+      && urlData[key] !== ''
+      && urlData[key] !== ','
+      && urlData[key] !== undefined
+      && urlData[key].length !== 0
+    ) {
+      // query search
+      search += `${key}=${urlData[key]}&`;
+    }
+  });
+  return search.substring(0, search.length - 1);
+}
+
+export function setStateToUrl(state) {
+  let urlData = {};
+  Object.keys(state).forEach((key) => {
+    //   dùng call tránh violate esLint, bỏ call để hiểu thêm
+    // if (state.hasOwnProperty.call(key, state)) {
+    // Validate các cases để tránh render query phù hợp, tránh âm, query=0|NULL
+    switch (key) {
+      case 'minPrice':
+        if (state[key] && state[key] > 0) {
+          urlData[key] = state[key];
+        } else {
+          urlData[key] = '';
+        }
+        break;
+      case 'maxPrice':
+        if (state[key] && state[key] < 100) {
+          urlData[key] = state[key];
+        } else {
+          urlData[key] = '';
+        }
+        break;
+      case 'amenities':
+        urlData[key] = state[key] && typeof state[key] !== 'string' && state[key].length
+          ? state[key].join()
+          : state[key];
+        break;
+      case 'room':
+        if (state[key]) {
+          urlData[key] = state[key] ? state[key] : 0;
+        } else {
+          urlData[key] = '';
+        }
+        break;
+      case 'guest':
+        if (state[key]) {
+          urlData[key] = state[key] ? state[key] : 0;
+        } else {
+          urlData[key] = '';
+        }
+        break;
+      case 'property':
+        urlData[key] = state[key] && typeof state[key] !== 'string' && state[key].length
+          ? state[key].join()
+          : state[key];
+        break;
+      case 'location':
+        if (state[key] && state[key].location_lat) {
+          urlData[`${key}_lat`] = state[key].location_lat;
+        }
+        if (state[key] && state[key].location_lng) {
+          urlData[`${key}_lng`] = state[key].location_lng;
+        }
+        break;
+      case 'reset':
+        urlData = state[key];
+        break;
+
+      default:
+        urlData[key] = state[key];
+        break;
+    }
+    // }
+  });
+  return createUrl(urlData);
+}
