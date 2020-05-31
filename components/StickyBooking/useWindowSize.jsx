b9ac059040f57f58f729b4615db028ea9c3bfd36
php1301 <19520854@gm.uit.edu.vn>
2020-05-31 23:26:34 +0700

    customHooks useWindowSize added

---
+++ components/StickyBooking/useWindowSize.jsx
@@ -0,0 +1,31 @@
+// Tham khảo từ https://usehooks.com/useWindowSize/
+
+import { useState, useEffect } from 'react';
+
+function getSize() {
+  return {
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+    outerHeight: window.outerHeight,
+    outerWidth: window.outerWidth,
+  };
+}
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState(getSize());
+
+  function handleResize() {
+    setWindowSize(getSize());
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowSize;
+};
+
+export default useWindowSize;
