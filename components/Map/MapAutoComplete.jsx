285cbda227579a1e25226659080ab4e4ad92c032
php1301 <19520854@gm.uit.edu.vn>
2020-06-08 21:51:32 +0700

    MapAutoComplete with recompose added

---
+++ components/Map/MapAutoComplete.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useRef } from 'react';
+import { compose, withProps } from 'recompose';
+import { withScriptjs } from 'react-google-maps';
+import StandaloneSearchBox from 'react-google-maps/lib/components/places/StandaloneSearchBox';
+
+import Input from '../UI/Antd/Input/Input';
+
+const SearchInput = compose(
+  // Snippet của react-google-maps, custom lại
+  // https://tomchentw.github.io/react-google-maps/#!/Documentation
+  withProps({
+    googleMapURL: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+    loadingElement: <div style={{ height: '100%' }} />,
+    containerElement: <div style={{ height: '400px' }} />,
+  }),
+  withScriptjs,
+)((props) => {
+  const { getInputValue } = props;
+  const [locationInput, setLocationInput] = useState({
+    searchedLocation: '',
+    searchedPlaceAPIData: [],
+  });
+  const handleOnChange = (event) => {
+    //   '13' === 'enter'
+    if (event.which === '13') {
+      event.preventDefault();
+      event.stopPropagation();
+      // stop render thêm
+    }
+    setLocationInput({
+      searchedLocation: event.target.value,
+    });
+  };
+  const refs = useRef({});
+  const onPlacesChanged = () => {
+    // getPlaces API của googlemaps
+    const places = refs.current.getPlaces();
+    // set data khi chọn cho ô input
+    setLocationInput({
+      searchedLocation: places && places[0] && places[0].formatted_address,
+      searchedPlaceAPIData: places || [], // places ? places : []
+    });
+    getInputValue({
+      searchedLocation: places && places[0] && places[0].formatted_address,
+      searchedPlaceAPIData: places || [], // places ? places : []
+    });
+  };
+  const handleOnPressEnter = (event) => {
+    if (event.which === '13') {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setLocationInput({ searchedLocation: event.target.value });
+    getInputValue(locationInput);
+  };
+  return (
+    <div className="map_autocomplete">
+      <StandaloneSearchBox ref={refs} onPlacesChanged={onPlacesChanged}>
+        <Input
+          type="text"
+          defaultValue=""
+          value={locationInput.searchedLocation || ''}
+          placeholder="Search “Vietnam, Asia”"
+          size="large"
+          onChange={handleOnChange}
+          onPressEnter={handleOnPressEnter}
+        />
+      </StandaloneSearchBox>
+    </div>
+  );
+});
+const MapAutoComplete = (props) => {
+  const { updateValue } = props;
+  return <SearchInput getInputValue={updateValue} />;
+};
+
+export default MapAutoComplete;
