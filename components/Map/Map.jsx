3a2165783378d466c7d83ccaddc18b63e73942ec
php1301 <19520854@gm.uit.edu.vn>
2020-06-08 21:56:41 +0700

    Map with condition render for Single and Listing - recompose

---
+++ components/Map/Map.jsx
@@ -0,0 +1,95 @@
+// chủ yếu xử lý Marker
+// customize từ https://tomchentw.github.io/react-google-maps/#!/MarkerClusterer/1
+
+import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import {
+  compose, withProps, withHandlers, withStateHandlers,
+} from 'recompose';
+import MarkerClusterer from 'react-google-maps/lib/components/addons/MarkerClusterer';
+
+import HotelMapMarkerCluster from './ListingPageMap';
+import HotelMapMarkerSingle from './SinglePageMap';
+
+import { MAPSTYLE } from './MapStyle';
+
+// div của map
+export const MapLoadingElement = (mapLoadingProps) => {
+  const { height } = mapLoadingProps;
+  return <div style={{ height }} />;
+};
+
+export const mapRenderData = {
+  // url để lấy
+  googleMapURL: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+  // Data load
+  loadingElement: <MapLoadingElement height="100%" />,
+  // container
+  containerElement: <div style={{ height: '400px' }} />,
+  mapElement: <div style={{ height: '400px' }} />,
+};
+
+export const MapDataProcessing = compose(
+  withProps(mapRenderData),
+  withHandlers({
+    onMarkerClustererClick: () => (markerClusterer) => {
+      const clickedMarkers = markerClusterer.getMarkers();
+      console.log(`Current clicked markers length: ${clickedMarkers.length}`);
+      console.log(clickedMarkers);
+    },
+  }),
+  // Xử lý state với recompose
+  withStateHandlers(
+    () => ({
+      isOpen: false,
+    }),
+    {
+      infoWindowToggle: ({ isOpen }) => (index) => ({
+        isOpen: !isOpen,
+        markerIndex: index,
+      }),
+    },
+  ),
+  withScriptjs,
+  withGoogleMap,
+)((props) => {
+  const { multiple } = props;
+  return (
+    <>
+      {multiple && multiple === true ? (
+        <GoogleMap
+          styles={MAPSTYLE}
+          defaultZoom={7}
+          defaultCenter={{
+            lat: 41.8858,
+            lng: -75.7223,
+          }}
+        >
+          <MarkerClusterer
+            onClick={props.onMarkerClustererClick}
+            averageCenter
+            defaultEnableRetinaIcons
+            gridSize={60}
+          >
+            <HotelMapMarkerCluster {...props} />
+          </MarkerClusterer>
+        </GoogleMap>
+      ) : (
+        <GoogleMap
+          styles={MAPSTYLE}
+          defaultZoom={8}
+          defaultCenter={{
+            lat: 40.706877,
+            lng: -74.011265,
+          }}
+        >
+          <HotelMapMarkerSingle {...props} />
+        </GoogleMap>
+      )}
+    </>
+  );
+});
+
+const Map = ({ children }) => <>{children}</>;
+
+export default Map;
