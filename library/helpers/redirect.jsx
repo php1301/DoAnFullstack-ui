79a19d1ac985a0a831305e3bf51ec4228d42c0e4
php1301 <19520854@gm.uit.edu.vn>
2020-06-11 23:46:59 +0700

    Redirect helpers added

---
+++ library/helpers/redirect.jsx
@@ -0,0 +1,17 @@
+import Router from 'next/router';
+
+/**
+ * Redirect to any given url
+ */
+export default (ctx = {}, target) => {
+  if (ctx.res) {
+    // Nếu đang SSR, trả về HTTP 303 và response Location
+    // Dùng để redirect.
+    ctx.res.writeHead(303, { Location: target });
+    ctx.res.end();
+  } else {
+    // Sử dụng Replace của Router (next) để replace cái
+    // Location của cái mới, xóa khỏi history (history.push)
+    Router.replace(target);
+  }
+};
