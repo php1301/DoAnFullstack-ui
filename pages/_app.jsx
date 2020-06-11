dac00a29bdac8c25fa25a3161161d2ab212a3ec3
php1301 <19520854@gm.uit.edu.vn>
2020-06-11 23:45:32 +0700

    _app.jsx config

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,8 +4,9 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../themes/default.theme';
 import GlobalStyles from '../assets/style/Global.style';
 import Layout from '../container/Layout/Layout';
-
+import { withData } from 'library/helpers/restriction';
 import { SearchProvider } from 'context/SearchProvider';
+import AuthProvider from 'context/AuthProvider';
 
 export default class CustomApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -14,28 +15,32 @@ export default class CustomApp extends App {
       pageProps = await Component.getInitialProps(ctx);
     }
     const { query, pathname } = ctx;
-
+    const { user, isLoggedIn } = withData(ctx);
     return {
-      pageProps, query, pathname,
+      pageProps, query, pathname, user, isLoggedIn,
     };
-  } // getInitialProps deprecrated
+  } // getInitialProps về các bản sau 9.3 không dc khuyến khích
 
   render() {
-    const { Component, pageProps, query } = this.props;
+    const {
+      Component, pageProps, query, user, isLoggedIn,
+    } = this.props;
     return (
-      <SearchProvider query={query}>
-        {/* // Render cứng header và footer - */}
-        <Layout>
-          {/* Các đoạn code ở dưới là children bao gồm cả các pages default index.js */}
-          <ThemeProvider theme={theme}>
-            <>
-              <GlobalStyles />
-              {/* Các pages - data */}
-              <Component {...pageProps} />
-            </>
-          </ThemeProvider>
-        </Layout>
-      </SearchProvider>
+      <AuthProvider>
+        <SearchProvider query={query}>
+          {/* // Render cứng header và footer - */}
+          <Layout user={user} isLoggedIn={isLoggedIn}>
+            {/* Các đoạn code ở dưới là children bao gồm cả các pages default index.js */}
+            <ThemeProvider theme={theme}>
+              <>
+                <GlobalStyles />
+                {/* Các pages - data */}
+                <Component isLoggedIn={isLoggedIn} {...pageProps} />
+              </>
+            </ThemeProvider>
+          </Layout>
+        </SearchProvider>
+      </AuthProvider>
     );
   }
 }
