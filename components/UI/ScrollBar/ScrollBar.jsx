e58eb71bad09973abffbb47ae866b391d7e44135
php1301 <19520854@gm.uit.edu.vn>
2020-06-13 20:01:41 +0700

    ScrollBar added

---
+++ components/UI/ScrollBar/ScrollBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+import { Wrapper, InnerWrapper } from './ScrollBar.style';
+
+export default function ScrollBar({
+  menu = [], other, height, className,
+}) {
+  // Add all classs to an array
+  const addAllClasses = ['scrollbar'];
+
+  // className prop checking
+  if (className) {
+    addAllClasses.push(className);
+  }
+  return (
+    <Wrapper className={addAllClasses.join(' ')} height={height}>
+      <InnerWrapper>
+        <div className="scrollbar_left">
+          {menu.map((menuItem) => (
+            <Link
+              key={menuItem.target}
+              className="linkItem"
+              activeClass="active"
+              to={menuItem.target}
+              spy
+              smooth
+              hashSpy
+              offset={-170}
+              duration={600}
+              isDynamic
+              ignoreCancelEvents={false}
+            >
+              {menuItem.name}
+            </Link>
+          ))}
+        </div>
+        <div className="scrollbar_right">{other && other}</div>
+      </InnerWrapper>
+    </Wrapper>
+  );
+}
