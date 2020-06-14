62d596f054beabf0cac6a9bd174151e73e0dd578
php1301 <19520854@gm.uit.edu.vn>
2020-06-14 20:54:43 +0700

    Clean export default for 2 User-related pages added

---
+++ container/User/index.jsx
@@ -0,0 +1,7 @@
+export {
+  default as UserAccountSettingsPage,
+} from './AccountSettings/UserAccountSettingsPage';
+
+export {
+  default as UserDetailsPage,
+} from './AccountDetails/UserDetailsPage';
