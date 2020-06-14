08249a1eb03f938139fa94b30ae449c7e1151cf5
php1301 <19520854@gm.uit.edu.vn>
2020-06-14 20:57:26 +0700

    SignInForm component added

---
+++ components/SignIn/RenderSignInForm.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Field, Form } from 'formik';
+import Link from 'next/link';
+import { MdLockOpen } from 'react-icons/md';
+import { AntInput, AntSwitch } from 'components/UI/Antd/AntdInputWithFormik';
+import Button from 'components/UI/Antd/Button/Button';
+
+import FormWrapper, {
+  FieldWrapper,
+  SwitchWrapper,
+  Label,
+} from './SignInForm.style';
+
+const RenderBasicInfoForm = (props) => {
+  console.log(props, 'formprops');
+
+  const {
+    values, submitCount, handleSubmit, forgetPasswordLink,
+  } = props;
+  return (
+    <FormWrapper>
+      <Form onSubmit={handleSubmit}>
+        <Field
+          component={AntInput}
+          name="email"
+          type="email"
+          size="large"
+          placeholder="somebody@somebody.io"
+          defaultValue={values.email}
+          submitCount={submitCount}
+          hasFeedback
+        />
+        <Field
+          component={AntInput}
+          name="password"
+          type="password"
+          size="large"
+          placeholder="Password"
+          submitCount={submitCount}
+          hasFeedback
+        />
+        <FieldWrapper className="field-container">
+          <SwitchWrapper>
+            <Field
+              component={AntSwitch}
+              name="rememberMe"
+              submitCount={submitCount}
+              defaultValue={values.rememberMe}
+            />
+            <Label>Remember Me</Label>
+          </SwitchWrapper>
+          <Link href={forgetPasswordLink}>
+            <a>Forget Password ?</a>
+          </Link>
+        </FieldWrapper>
+
+        <Button
+          className="signin-btn"
+          type="primary"
+          htmlType="submit"
+          size="large"
+          style={{ width: '100%' }}
+        >
+          <MdLockOpen />
+          Login
+        </Button>
+      </Form>
+    </FormWrapper>
+  );
+};
+
+export default RenderBasicInfoForm;
