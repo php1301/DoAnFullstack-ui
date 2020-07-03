1e40a311032b16d40d1fc1225a77c9f9aae375ef
php1301 <19520854@gm.uit.edu.vn>
2020-07-03 21:59:25 +0700

    Mutations added

--- apollo-graphql/mutation/mutation.jsx
+++ apollo-graphql/mutation/mutation.jsx
@@ -19,3 +19,78 @@ export const LOGIN = gql`
         }
     }
 `;
+
+export const SIGNUP = gql`
+    mutation Signup($signUpInput:SignUpInput){
+        signup(signUpInput: $signUpInput){
+            id
+            email
+            last_name
+            first_name
+            profile_pic_main
+        }
+    }
+`;
+
+export const LIKE = gql`
+    mutation LikeHotel($id: ID!){
+        likeHotel(id:$id){
+            title
+            # id
+            # peopleLiked
+            # connectId
+            # agentId
+            # title
+            # slug
+            # status
+            # content
+            # price
+            # isNegotiable
+            # propertyType
+            # condition
+            # rating
+            # ratingCount
+            # contactNumber
+            # termsAndCondition
+            # amenities
+            # image
+            # location
+            # gallery
+            # categories
+            # reviews
+            # createdAt
+            # updatedAt
+        }
+    }
+`;
+export const DISLIKE = gql`
+    mutation DislikeHotel($id: ID!){
+        dislikeHotel(id:$id){
+            title
+            # id
+            # peopleLiked
+            # connectId
+            # agentId
+            # title
+            # slug
+            # status
+            # content
+            # price
+            # isNegotiable
+            # propertyType
+            # condition
+            # rating
+            # ratingCount
+            # contactNumber
+            # termsAndCondition
+            # amenities
+            # image
+            # location
+            # gallery
+            # categories
+            # reviews
+            # createdAt
+            # updatedAt
+        }
+    }
+`;
