c9928b167eda6efd588b4efc46b01add2bbbcd49
php1301 <19520854@gm.uit.edu.vn>
2020-07-07 21:58:59 +0700

    Mutations added

--- apollo-graphql/mutation/mutation.jsx
+++ apollo-graphql/mutation/mutation.jsx
@@ -8,6 +8,7 @@ export const CREATE_HOTEL = gql`
         }
     }
 `;
+
 export const LOGIN = gql`
     mutation Login($loginInput: LoginInput){
         login(loginInput: $loginInput){
@@ -16,10 +17,10 @@ export const LOGIN = gql`
             last_name
             first_name
             profile_pic_main
+            cover_pic_main
         }
     }
 `;
-
 export const SIGNUP = gql`
     mutation Signup($signUpInput:SignUpInput){
         signup(signUpInput: $signUpInput){
@@ -28,6 +29,14 @@ export const SIGNUP = gql`
             last_name
             first_name
             profile_pic_main
+            cover_pic_main
+        }
+    }
+`;
+export const UPDATE_PASSWORD = gql`
+    mutation UpdatePassword($password: UpdatePassword){
+        updatePassword(password:$password){
+            email
         }
     }
 `;
@@ -91,6 +100,67 @@ export const DISLIKE = gql`
             # reviews
             # createdAt
             # updatedAt
+        }
+    }
+`;
+
+export const UPDATE_PROFILE = gql` 
+
+    mutation UpdateProfile($profile: UpdateProfileInput, $location: LocationInput, $social: SocialInput){
+    updateProfile(profile: $profile, location: $location, social: $social){
+    first_name
+    last_name
+    date_of_birth
+    gender
+    agent_location {
+      lat
+      lng
+      formattedAddress
+      zipcode
+      city
+      state_long
+      state_short
+      country_long
+      country_short
+    }
+    social_profile {
+      facebook
+      linkedIn
+      twitter
+      instagram
+    }
+    cellNumber
+    content
+        }
+    }
+`;
+export const UPDATE_PHOTOS = gql`
+    mutation UpdatePhotos($photos: [UpdatePhotosInput]){
+        updatePhotos(photos: $photos){
+            email
+            # profile_pic_main
+            # cover_pic_main
+        }
+    }
+`;
+export const DELETE_PHOTOS = gql`
+    mutation DeletePhotos($photos: [DeletePhotosInput]){
+        deletePhotos(photos: $photos){
+            email
+        }
+    }
+`;
+export const SET_PROFILE_PIC = gql`
+    mutation SetProfilePic($url: String){
+        setProfilePic(url: $url){
+            email
+        }
+    }
+`;
+export const SET_COVER_PIC = gql`
+    mutation SetCoverPic($url: String){
+        setCoverPic(url: $url){
+            email
         }
     }
 `;
