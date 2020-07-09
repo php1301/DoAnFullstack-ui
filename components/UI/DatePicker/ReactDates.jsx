bb0a8a0cf658ecd49d0df337c9b72602245dd842
php1301 <19520854@gm.uit.edu.vn>
2020-07-09 22:12:53 +0700

    React Dates formatted

--- components/UI/DatePicker/ReactDates.jsx
+++ components/UI/DatePicker/ReactDates.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/destructuring-assignment */
 
 // Snippet và custom react-dates snippet
@@ -5,7 +6,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
 import { ReactDatesStyleWrapper } from './ReactDates.style';
 /*
  * Để áp dụng GPS enable code dưới [example : Vietnam]
@@ -13,7 +13,7 @@ import { ReactDatesStyleWrapper } from './ReactDates.style';
 // import moment from 'moment';
 // import 'moment/locale/vi';
 
-class ReactDates extends Component {
+class DateRangePickerBox extends Component {
   constructor(props) {
     super(props);
     const separator = this.props.item && this.props.item.separator
@@ -37,11 +37,120 @@ class ReactDates extends Component {
     // moment.locale('fr');
   }
 
+  onDateChangeFunc = ({ startDate, endDate }) => {
+    const { dateFormat } = this.state;
+    this.setState({ startDate, endDate });
+    if (startDate !== null && endDate !== null) {
+      this.props.updateSearchData({
+        setStartDate: startDate.format(dateFormat),
+        setEndDate: endDate.format(dateFormat),
+      });
+    }
+  };
+
+  onFocusChangeFunc = (focusedInput) => this.setState({ focusedInput });
+
   render() {
+    const { focusedInput, startDate, endDate } = this.state;
+    // DateRangePickerBox props list
+    const {
+      className,
+      startDateId,
+      endDateId,
+      startDatePlaceholderText,
+      endDatePlaceholderText,
+      disabled,
+      showClearDates,
+      isRTL,
+      orientation,
+      anchorDirection,
+      withPortal,
+      withFullScreenPortal,
+      small,
+      block,
+      numberOfMonths,
+      regular,
+      noBorder,
+    } = this.props;
+
+    // Add all classs to an array **************
+    const addAllClasses = ['date_picker'];
+    // className prop checking **************
+    if (className) {
+      addAllClasses.push(className);
+    }
+
+    // React-Dates DateRangePicker Props List
+    const defaultCalendarProps = {
+      startDateId: startDateId || 'start_unique_id',
+      endDateId: endDateId || 'end_date_unique_id',
+      startDate,
+      endDate,
+      focusedInput,
+      onFocusChange: this.onFocusChangeFunc,
+      onDatesChange: this.onDateChangeFunc,
+      startDatePlaceholderText,
+      endDatePlaceholderText,
+      disabled,
+      isRTL,
+      showClearDates: showClearDates || false,
+      orientation,
+      anchorDirection,
+      withPortal,
+      withFullScreenPortal,
+      small,
+      numberOfMonths: numberOfMonths || 2,
+      block,
+      regular,
+      noBorder,
+    };
+
     return (
-      <div />
+      <ReactDatesStyleWrapper className={addAllClasses.join(' ')}>
+        <DateRangePicker {...defaultCalendarProps} />
+      </ReactDatesStyleWrapper>
     );
   }
 }
 
-export default ReactDates;
+DateRangePickerBox.propTypes = {
+  /** startDateId của the date-picker field */
+  startDateId: PropTypes.string.isRequired,
+  /** endDateId của the date-picker field */
+  endDateId: PropTypes.string.isRequired, //
+  /** startDatePlaceholderText của the date-picker field */
+  startDatePlaceholderText: PropTypes.string,
+  /** endDatePlaceholderText của the date-picker field */
+  endDatePlaceholderText: PropTypes.string,
+  /** disabled của the date-picker field */
+  disabled: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.oneOf(['START_DATE', 'END_DATE']),
+  ]),
+
+  /** showClearDates của the date-picker field */
+  showClearDates: PropTypes.bool,
+  /** isRTL của the date-picker field */
+  isRTL: PropTypes.bool,
+  /** orientation của the date-picker field */
+  orientation: PropTypes.oneOf(['horizontal', 'vertical']),
+  /** anchorDirection của the date-picker field */
+  anchorDirection: PropTypes.oneOf(['left', 'right']),
+  /** withPortal của the date-picker field */
+  withPortal: PropTypes.bool,
+  /** withFullScreenPortal của the date-picker field */
+  withFullScreenPortal: PropTypes.bool,
+  /** small của the date-picker field */
+  small: PropTypes.bool,
+  /** numberOfMonths của the date-picker field */
+  numberOfMonths: PropTypes.number,
+  /** block của the date-picker field */
+  block: PropTypes.bool,
+  /** regular của the date-picker field */
+  regular: PropTypes.bool,
+  /** noBorder của the date-picker field */
+  noBorder: PropTypes.bool,
+  /** updateSearchData của the date-picker field để gửi data của component đến parent component */
+  updateSearchData: PropTypes.func,
+};
+export default DateRangePickerBox;
