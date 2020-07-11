d2534371d57c50652f00bbc4a2e6e2c403385bc4
php1301 <19520854@gm.uit.edu.vn>
2020-07-11 20:53:21 +0700

    Session context added

--- library/helpers/session.jsx
+++ library/helpers/session.jsx
@@ -7,15 +7,18 @@ export const TOKEN_COOKIE = 'token';
 export const USER_COOKIE = 'user';
 
 // Cho client khi CSR
-const getCookieFromBrowser = (key) => cookie.get(key);
+// Ở đây khi làm BE đã parse vô luôn browser
+export const getCookieFromBrowser = (key) => cookie.get(key);
 
 // Khi đang SSR - genrate = nextCookie, gắn vô key 'id_token' trong tab cookie
-
-const getCookieFromServer = (ctx, key = 'id_token') => {
+// Dành cho monorepo hoặc không parse cookie
+// Xử lý refresh page lúc process.browser chưa nhận
+const getCookieFromServer = (ctx, key) => {
+  const specifiKey = key === 'token' ? 'token' : 'user';
   const cookieServer = nextCookie(ctx);
-  const token = cookieServer && cookieServer[key] ? cookie[key] : false;
+  const token = cookieServer && cookieServer[specifiKey] ? cookieServer[specifiKey] : false;
   if (!token) return null;
-  return token;
+  return JSON.stringify(token);
 };
 
 // Get Cookie và set context phù hợp cho CSR (chỉ cần key token để auth)
