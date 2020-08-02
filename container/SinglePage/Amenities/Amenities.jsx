a2a59257270a8f19bcd2becfc036ada24bad83fe
php1301 <19520854@gm.uit.edu.vn>
2020-08-02 23:27:26 +0700

    Amenities slug updated

--- container/SinglePage/Amenities/Amenities.jsx
+++ container/SinglePage/Amenities/Amenities.jsx
@@ -3,22 +3,37 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Heading from 'components/UI/Heading/Heading';
 import {
-  FaWifi, FaCarAlt, FaSwimmer, FaAirFreshener,
+  FaWifi, FaCarAlt, FaSwimmer, FaAirFreshener, FaBan,
 } from 'react-icons/fa';
+import {
+  FiWifiOff,
+} from 'react-icons/fi';
+import {
+  GiPoolDive,
+  GiDespair,
+} from 'react-icons/gi';
 import IconCard from 'components/IconCard/IconCard';
 import AmenitiesWrapper, { AmenitiesArea } from './Amenities.style';
 import { TextButton } from '../SinglePageView.style';
 import { Element } from 'react-scroll';
 
-const Amenities = ({ titleStyle, linkStyle }) => (
+const Amenities = ({ titleStyle, linkStyle, amenities }) => (
   <Element name="amenities" className="Amenities">
     <AmenitiesWrapper>
       <Heading as="h2" content="Amenities" {...titleStyle} />
       <AmenitiesArea>
-        <IconCard icon={<FaWifi />} title="Free wifi" />
-        <IconCard icon={<FaCarAlt />} title="Free parking" />
-        <IconCard icon={<FaSwimmer />} title="Free pool" />
-        <IconCard icon={<FaAirFreshener />} title="Air Freshener" />
+        {amenities[0].wifiAvailability
+          ? <IconCard icon={<FaWifi />} title="Free wifi" />
+          : <IconCard icon={<FiWifiOff />} title="No free wifi" />}
+        {amenities[0].parkingAvailability
+          ? <IconCard icon={<FaCarAlt />} title="Free parking" />
+          : <IconCard icon={<FaBan />} title="No Free parking" />}
+        {amenities[0].poolAvailability
+          ? <IconCard icon={<FaSwimmer />} title="Free pool" />
+          : <IconCard icon={<GiPoolDive />} title="No Free pool" />}
+        {amenities[0].airCondition
+          ? <IconCard icon={<FaAirFreshener />} title="Air Condition" />
+          : <IconCard icon={<GiDespair />} title="No Air Condition" />}
       </AmenitiesArea>
       <TextButton>
         <Link href="#1">
