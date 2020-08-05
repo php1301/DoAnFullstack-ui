5e5f911c0fdae8101beb12e447eeda264f7ddc8b
php1301 <19520854@gm.uit.edu.vn>
2020-08-05 23:05:02 +0700

    TopBar modified

--- container/SinglePage/TopBar/TopBar.jsx
+++ container/SinglePage/TopBar/TopBar.jsx
@@ -54,35 +54,40 @@ const SocialShareMenu = (props) => {
   );
 };
 
-const SideButtons = (props) => (
-  <ButtonGroup>
-    <Favorite className="ant-btn" content="Save" />
-    <Dropdown
-      placement="bottomRight"
-      overlay={() => <SocialShareMenu {...props} />}
-      overlayClassName="social_share_menu"
-    >
-      <Button className="ant-dropdown-link">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.309 15.537">
-          <path
-            d="M80.68,101.873,74.507,96.1a.316.316,0,0,0-.245-.105c-.193.009-.438.144-.438.35v2.9a.187.187,0,0,1-.158.179c-6.138.941-8.724,5.535-9.639,10.3-.035.188.219.363.337.214a11.158,11.158,0,0,1,9.275-4.7.216.216,0,0,1,.184.21v2.844a.375.375,0,0,0,.634.232l6.217-5.876a.483.483,0,0,0,.153-.368A.586.586,0,0,0,80.68,101.873Z"
-            transform="translate(-63.271 -95.242)"
-          />
-        </svg>
-        Share
-      </Button>
-    </Dropdown>
-  </ButtonGroup>
-);
+const SideButtons = (props) => {
+  const { id, heart } = props;
+  return (
+    <ButtonGroup>
+      <Favorite id={id} heart={heart} className="ant-btn" content="Save" />
+      <Dropdown
+        placement="bottomRight"
+        overlay={() => <SocialShareMenu {...props} />}
+        overlayClassName="social_share_menu"
+      >
+        <Button className="ant-dropdown-link">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.309 15.537">
+            <path
+              d="M80.68,101.873,74.507,96.1a.316.316,0,0,0-.245-.105c-.193.009-.438.144-.438.35v2.9a.187.187,0,0,1-.158.179c-6.138.941-8.724,5.535-9.639,10.3-.035.188.219.363.337.214a11.158,11.158,0,0,1,9.275-4.7.216.216,0,0,1,.184.21v2.844a.375.375,0,0,0,.634.232l6.217-5.876a.483.483,0,0,0,.153-.368A.586.586,0,0,0,80.68,101.873Z"
+              transform="translate(-63.271 -95.242)"
+            />
+          </svg>
+          Share
+        </Button>
+      </Dropdown>
+    </ButtonGroup>
+  );
+};
 
 const TopBar = (props) => {
-  const { title, author, media } = props;
+  const {
+    title, agentName, media, id, heart,
+  } = props;
   return (
     <TobBarWrapper>
       <Sticky innerZ={9999} top={82} activeClass="isSticky">
         <ScrollBar
           menu={topBarMenu}
-          other={<SideButtons media={media} author={author} title={title} />}
+          other={<SideButtons media={media} agentName={agentName} title={title} id={id} heart={heart} />}
         />
       </Sticky>
     </TobBarWrapper>
