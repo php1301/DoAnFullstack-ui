62baf186a881481ef0971c634091e5c2cd97df35
php1301 <19520854@gm.uit.edu.vn>
2020-08-08 23:14:29 +0700

    ApolloProvider config fixed for SSR and WS

--- apollo-graphql/ApolloProvider.jsx
+++ apollo-graphql/ApolloProvider.jsx
@@ -3,24 +3,59 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { getMainDefinition } from 'apollo-utilities'
+import { split } from 'apollo-link'
 import fetch from 'node-fetch';
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/graphql',
-  fetch,
+// const client = new SubscriptionClient(`ws://localhost:3000/graphql`, {
+//   reconnect: true
+// });
+
+// const link = new WebSocketLink(client);
+const wsLink = process.browser ? new WebSocketLink({ // Vì ssr nên browser sẽ throw phải check null
+  uri: `ws://localhost:3000/graphql`,
+  options: {
+    reconnect: true,
+    lazy: true,
+  },
+  onReconnected:()=>{
+    console.log("WS reconnected")
+  },
+  onConnected:()=>{
+    console.log("WS connect");
+  },
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log('graphQLErrors + Socket error', graphQLErrors);
+    console.log('networkError + Socket error', networkError);
+  },
+}) : null;
+
+const httplink = new createHttpLink({
+	uri: 'http://localhost:3000/graphql',
   credentials: 'include',
+  fetch,
   onError: ({ networkError, graphQLErrors }) => {
     console.log('graphQLErrors', graphQLErrors);
     console.log('networkError', networkError);
   },
 });
-// https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#login_usage nếu muốn gắn vào header
 
+const link = process.browser ? split( //Chỉ split khi trên browser (đủ 2 links)
+  // split dựa trên loại connection
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httplink,
+) : httplink;
 const client = new ApolloClient({
-  link: httpLink,
+  link,
+  ssrMode: true,
   cache: new InMemoryCache(),
 });
-
 export const ApolloComponent = (props) => {
   const { children } = props;
   return (<ApolloProvider client={client}>{children}</ApolloProvider>);
