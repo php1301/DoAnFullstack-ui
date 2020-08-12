6c8a8243defbc34455f989f0aa6eb9351eadded2
php1301 <19520854@gm.uit.edu.vn>
2020-08-12 22:49:29 +0700

    subsriptions added

--- apollo-graphql/subscription/subscription.jsx
+++ apollo-graphql/subscription/subscription.jsx
@@ -11,13 +11,59 @@ export const UNREAD_NOTIFICATION = gql`
 export const NOTIFICATION_BELL = gql`
     subscription NotificationBell($channelId: String){
         notificationBell(channelId: $channelId) {
-            peopleReviewedQuanity
+            peopleReviewedQuantity
             reviewAuthorName
+            query
             reviewTitle
+            read
             reviewText
-            reviewedAuthorId
             reviewedHotelName
-
-  }
+            reviewAuthorProfilePic
+        }
+    }
+`;
+export const REALTIME_REVIEWS = gql`
+    subscription RealtimeReviews($hotelId: String){
+        realtimeReviews(hotelId: $hotelId){
+        reviewID
+        reviewTitle
+        reviewText
+        sortOfTrip
+        reviewAuthorId{
+          id
+        }
+        reviewAuthorFirstName
+        reviewAuthorLastName
+        reviewAuthorEmail
+        reviewOverall
+        reviewAuthorPic
+        reviewedHotelId
+        reviewTips
+        reviewPics{
+           url
+        }
+        reviewDate
+        reviewOptional{
+          option 
+          optionField
+        }
+        reviewFields{
+            rating 
+            ratingFieldName
+          }
+    }
+}
+`;
+export const REALTIME_LIKE_DISLIKE = gql`
+    subscription RealtimeLikeDislike($reviewID: String){
+        realtimeLikeDislike(reviewID: $reviewID){
+            reviewID
+            peopleLiked{
+                id
+            }
+            peopleDisliked{
+                id
+            }
+        }
     }
 `;
