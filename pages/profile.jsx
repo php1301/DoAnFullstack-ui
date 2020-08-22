f3cf6ea185dda7d26104f13128525a5379122a80
php1301 <19520854@gm.uit.edu.vn>
2020-08-22 23:58:34 +0700

    Profile page added

---
+++ pages/profile.jsx
@@ -0,0 +1,33 @@
+import { UserDetailsPage } from 'container/User/index';
+// import GetAPIData, { ProcessAPIData } from 'library/helpers/get_api_data';
+import { secretPage } from 'library/helpers/restriction';
+import { getDeviceType } from 'library/helpers/get_device_type';
+
+const Profile = ({ processedData, ...props }) => (
+  <UserDetailsPage
+    // processedData={processedData.data}
+    {...props}
+  />
+);
+
+Profile.getInitialProps = async (context) => {
+  const { query, req } = context;
+  const isLoggedIn = secretPage(context);
+  // const apiUrl = [
+  //   {
+  //     endpoint: 'user',
+  //     name: 'listingUser',
+  //   },
+  // ];
+  // const { data, loading, error } = useQuery(GET_PROFILE_OF_CURRENT_USER,{})
+
+  // const pageData = await GetAPIData(apiUrl); // getApiData(data)
+  // const processedData = ProcessAPIData(pageData);
+  const deviceType = getDeviceType(req);
+
+  return {
+    query, isLoggedIn, deviceType,
+  };
+};
+
+export default Profile;
