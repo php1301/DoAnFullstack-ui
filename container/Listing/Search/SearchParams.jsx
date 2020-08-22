6e6071bcc17b956c6e90d8593eeb1fa5cd90a0a5
php1301 <19520854@gm.uit.edu.vn>
2020-08-22 23:50:33 +0700

    SearchParams modified priceInit

--- container/Listing/Search/SearchParams.jsx
+++ container/Listing/Search/SearchParams.jsx
@@ -2,7 +2,7 @@
 // default data cho CategorySearch
 export const priceInit = {
   0: '$0',
-  100: '$100',
+  1000: '$1000',
 };
 
 export const calendarItem = {
@@ -18,24 +18,40 @@ export const getAmenities = {
   options: [
     { label: 'Free Wi-Fi', value: 'free-wifi' },
     { label: 'Free Parking', value: 'free-parking' },
-    { label: 'Breakfast included', value: 'breakfast' },
+    { label: 'Breakfast included(x)', value: 'breakfast' },
     { label: 'Pool', value: 'pool' },
     { label: 'Air Conditioning', value: 'air-condition' },
-    { label: 'Hot Shower', value: 'hot-shower' },
-    { label: 'Cable TV', value: 'cable-tv' },
+    { label: 'Hot Shower(x)', value: 'hot-shower' },
+    { label: 'Cable TV(x)', value: 'cable-tv' },
   ],
 };
 
 export const getPropertyType = {
   id: 2,
   name: 'Property Type',
-  identifier: 'property-type',
+  identifier: 'propertyType',
   options: [
-    { label: 'Villa', value: 'villa' },
-    { label: 'Hotel', value: 'hotel' },
-    { label: 'Resort', value: 'resort' },
-    { label: 'Cottage', value: 'cottage' },
-    { label: 'Duplex', value: 'duplex' },
-    { label: 'Landscape', value: 'landscape' },
+    { label: 'Villa', value: 'Villa' },
+    { label: 'Hotel', value: 'Hotel' },
+    { label: 'Resort', value: 'Resort' },
+    { label: 'Cottage', value: 'Cottage' },
+    { label: 'Duplex', value: 'Duplex' },
+    { label: 'Landscape', value: 'Landscape' },
+  ],
+};
+
+export const sortListing = {
+  id: 6,
+  name: 'Sort',
+  identifier: 'sortListing',
+  options: [
+    { label: 'Rating ASC', value: 'rating_ASC' },
+    { label: 'Rating DESC', value: 'rating_DESC' },
+    { label: 'Created ASC', value: 'createdAt_ASC' },
+    { label: 'Created DESC', value: 'createdAt_DESC' },
+    { label: 'Reviews MOST', value: 'ratingCount_DESC' },
+    { label: 'Reviews LEAST', value: 'ratingCount_ASC' },
+    { label: 'Price ASC', value: 'price_ASC' },
+    { label: 'Price DESC', value: 'price_DESC' },
   ],
 };
