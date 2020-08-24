db2dd667e2adbb4a8f87f031d48d1977248b12e9
php1301 <19520854@gm.uit.edu.vn>
2020-08-24 23:59:22 +0700

    Role tag and TXID page added for MobileMenu

--- container/Layout/Header/MobileMenu.jsx
+++ container/Layout/Header/MobileMenu.jsx
@@ -7,15 +7,16 @@ import {
   LISTING_POSTS_PAGE,
   PRICING_PLAN_PAGE,
   USER_ACCOUNT_SETTINGS_PAGE,
+  SEARCH_TXID_PAGE,
 } from 'settings/constants';
 
 // có thể coi className như props
-const MobileMenu = ({ className }) => {
+const MobileMenu = ({ className, sidebarHandler }) => {
   // auth context
   const { loggedIn, logOut } = useContext(AuthContext);
 
   return (
-    <Menu className={className}>
+    <Menu onClick={() => { sidebarHandler(); }} className={className}>
       <Menu.Item key="0">
         <ActiveLink href={HOME_PAGE}>Hotels</ActiveLink>
       </Menu.Item>
@@ -25,16 +26,26 @@ const MobileMenu = ({ className }) => {
       <Menu.Item key="2">
         <ActiveLink href={PRICING_PLAN_PAGE}>Pricing</ActiveLink>
       </Menu.Item>
+      <Menu.Item key="3">
+        <ActiveLink href={SEARCH_TXID_PAGE}>TXID</ActiveLink>
+      </Menu.Item>
       {loggedIn && (
-        <Menu.Item key="3">
+        <Menu.Item key="4">
           <ActiveLink href={USER_ACCOUNT_SETTINGS_PAGE}>
             Account Settings
           </ActiveLink>
         </Menu.Item>
       )}
       {loggedIn && (
-        <Menu.Item key="4">
-          <button type="button" onClick={logOut}>Log Out</button>
+        <Menu.Item key="5">
+          <a
+            role="button"
+            tabIndex="-1"
+            onKeyDown={logOut}
+            onClick={logOut}
+          >
+            Log out
+          </a>
         </Menu.Item>
       )}
     </Menu>
