262d652a0fd2e922e2cd2fa579de63ae84298ab8
php1301 <19520854@gm.uit.edu.vn>
2020-08-31 21:00:47 +0700

    ForgetPasswordForm modified with mutation added

--- container/ForgetPassword/ForgetPasswordForm.jsx
+++ container/ForgetPassword/ForgetPasswordForm.jsx
@@ -1,29 +1,63 @@
 import React from 'react';
+import { useMutation } from 'react-apollo';
+import { toast } from 'react-toastify';
 import { Formik } from 'formik';
-import RenderForgetPasswordForm from 'components/ForgetPassword/RenderForgetPassswordForm';
 import * as Yup from 'yup';
+import RenderForgetPasswordForm from 'components/ForgetPassword/RenderForgetPassswordForm';
+import { FORGET_PASSWORD } from 'apollo-graphql/mutation/mutation';
 
+export default function ForgetPassWordForm() {
+  const [forgetPassword] = useMutation(FORGET_PASSWORD, {
+    onCompleted: () => {
+      toast.success('Email for resetting password sent!', {
+        position: 'top-left',
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    },
+  });
+  const initialValues = {
+    email: '',
+  };
 
-const initialValues = {
-  email: '',
-};
+  const getPasswordFormValidationSchema = () => Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid Email')
+      .required('Must enter Email'),
+  });
 
-const getPasswordFormValidationSchema = () => Yup.object().shape({
-  email: Yup.string()
-    .email('Invalid Email')
-    .required(',Must enter Email'),
-});
+  const handleSubmit = async (formProps) => {
+    const { email } = formProps;
+    try {
+      await forgetPassword({
+        variables: {
+          email,
+        },
+      });
+    } catch (e) {
+      toast.error(e.message.slice(15), {
+        position: 'top-left',
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
 
-const handleSubmit = (formProps) => {
-  const { email } = formProps;
-  alert(`Client email is ${email}`);
-};
-
-export default () => (
-  <Formik
-    initialValues={initialValues}
-    onSubmit={handleSubmit}
-    validationSchema={getPasswordFormValidationSchema}
-    render={RenderForgetPasswordForm}
-  />
-);
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={getPasswordFormValidationSchema}
+    >
+      {RenderForgetPasswordForm}
+    </Formik>
+  );
+}
