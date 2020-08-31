221b6f8b6a1a356a59ff9a54ff35e5f206639dd8
php1301 <19520854@gm.uit.edu.vn>
2020-08-31 20:18:32 +0700

    index modified commented code getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { Waypoint } from 'react-waypoint';
+// import { i18n, withTranslation } from '../i18n';
 
 import { getDeviceType } from 'library/helpers/get_device_type';
 import GetAPIData from 'library/helpers/get_api_data';
@@ -12,8 +13,7 @@ import SectionTitle from 'components/SectionTitle/SectionTitle';
 import SectionGrid from 'components/SectionGrid/SectionGrid';
 import SearchArea from 'container/Home/Search/Search';
 import LocationGrid from 'container/Home/Location/Location';
-
-
+import IntlMessages from 'library/helpers/i18n';
 import { LayoutContext } from 'context/LayoutProvider';
 import { LISTING_POSTS_PAGE, SINGLE_POST_PAGE } from 'settings/constants';
 import {
@@ -22,13 +22,17 @@ import {
   HOME_PAGE_SECTIONS_ITEM_LIMIT_FOR_DESKTOP_DEVICE,
   HOME_PAGE_SECTIONS_COLUMNS_RESPONSIVE_WIDTH,
 } from '../settings/config';
+// import Loader from 'components/Loader/Loader';
 // eslint-disable-next-line arrow-body-style
 const Home = ({
+  // t,
   deviceType,
   locationData,
   topHotelData,
   luxuryHotelData,
 }) => {
+  // if (typeof window === 'undefined') return <Loader />;
+  // const deviceType = getDeviceType();
   let limit;
   const [, dispatch] = useContext(LayoutContext);
 
@@ -67,14 +71,15 @@ const Home = ({
         />
         {/* Truyền param 1 post dạng grid */}
         <SectionGrid
-          link={SINGLE_POST_PAGE}
+          type="index"
+          link={LISTING_POSTS_PAGE}
           columnWidth={HOME_PAGE_SECTIONS_COLUMNS_RESPONSIVE_WIDTH}
           data={topHotelData.slice(0, limit)}
           limit={limit}
           deviceType={deviceType}
         />
         <SectionTitle
-          title={<Heading content="Top Luxury Hotel" />}
+          title={<Heading content="Top luxury hotel" />}
           link={(
             <Link href={LISTING_POSTS_PAGE}>
               <a>
@@ -83,8 +88,16 @@ const Home = ({
             </Link>
           )}
         />
+        <IntlMessages id="uiElements.badge.basicExampleSubTitle" />
+        {/* <button
+          type="button"
+          onClick={() =>  i18n.changeLanguage(i18n.language === 'en' ? 'de' : 'en'); }
+        >
+          {t('common.change-locale', { locale: 'VI' })}
+        </button> */}
         {/* Truyền param 1 post dạng grid */}
         <SectionGrid
+          type="index"
           link={SINGLE_POST_PAGE}
           columnWidth={HOME_PAGE_SECTIONS_COLUMNS_RESPONSIVE_WIDTH}
           data={luxuryHotelData.slice(0, limit)}
@@ -128,7 +141,57 @@ Home.getInitialProps = async ({ req }) => {
     });
   }
   return {
-    deviceType, locationData, topHotelData, luxuryHotelData,
+    deviceType, locationData, topHotelData, luxuryHotelData, namespacesRequired: ['common'],
   };
 };
+// export default withTranslation('common')(Home);
+
+// Nếu path là dynamic thì cần hàm dưới
+
+// export async function getStaticPaths() {
+//   return {
+//     paths: [
+//       { params: { ... } } // See the "paths" section below
+//     ],
+//     fallback: true or false // See the "fallback" section below
+//   };
+// }
+
+// export async function getStaticProps() {
+//   const apiUrl = [
+//     {
+//       endpoint: 'location',
+//       name: 'locationData',
+//     },
+//     {
+//       endpoint: 'top-hotel',
+//       name: 'topHotelData',
+//     },
+//     {
+//       endpoint: 'hotel',
+//       name: 'luxuryHotelData',
+//     },
+//   ];
+//   const pageData = await GetAPIData(apiUrl);
+//   let locationData = [];
+//   let topHotelData = [];
+//   let luxuryHotelData = [];
+//   if (pageData) {
+//     pageData.forEach((item) => {
+//       if (item.name === 'locationData') {
+//         // clone lại mảng item.data, có thể log ra để xem
+//         locationData = item.data ? [...item.data] : [];
+//       } else if (item.name === 'topHotelData') {
+//         topHotelData = item.data ? [...item.data] : [];
+//       } else if (item.name === 'luxuryHotelData') {
+//         luxuryHotelData = item.data ? [...item.data] : [];
+//       }
+//     });
+//   }
+//   return {
+//     props: {
+//       locationData, topHotelData, luxuryHotelData, namespacesRequired: ['common'],
+//     },
+//   };
+// }
 export default Home;
