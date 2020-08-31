e94e4a5607d1490b8ec06678daf23b429cba9eca
php1301 <19520854@gm.uit.edu.vn>
2020-08-31 20:19:51 +0700

    account-settings modified

--- pages/account-settings.jsx
+++ pages/account-settings.jsx
@@ -1,13 +1,25 @@
+import Router from 'next/router';
+import Head from 'next/head';
 import { UserAccountSettingsPage } from 'container/User/index';
 import GetAPIData, { ProcessAPIData } from 'library/helpers/get_api_data';
 import { secretPage } from 'library/helpers/restriction';
 import { getDeviceType } from 'library/helpers/get_device_type';
 
-const AccountSettings = (props) => (
-  <UserAccountSettingsPage
-    {...props}
-  />
-);
+const AccountSettings = (props) => {
+  if (typeof window !== 'undefined') { if (props.query.u) Router.replace('/account-settings'); }
+  return (
+    <>
+      <Head>
+        <title>
+          Account Settings
+        </title>
+      </Head>
+      <UserAccountSettingsPage
+        {...props}
+      />
+    </>
+  );
+};
 
 AccountSettings.getInitialProps = async (context) => {
   const { req, query } = context;
