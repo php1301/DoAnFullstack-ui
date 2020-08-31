9ae15e97f96c6f04251a7a5c967195f2103d215b
php1301 <19520854@gm.uit.edu.vn>
2020-08-31 20:58:20 +0700

    PriceCard and ProductCard selection with key

--- container/Pricing/PricingItems.jsx
+++ container/Pricing/PricingItems.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PriceCard from 'components/PriceCard/PriceCard';
 
-export default function PricingItems({ plans }) {
+export default function PricingItems({ user, plans }) {
   return plans.map((plan) => (
     <PriceCard
+      user={user}
       className="price_card"
       data={plan}
-      key={plan.title}
+      key={plan.key}
       buttonText="Select Plan"
     />
   ));
