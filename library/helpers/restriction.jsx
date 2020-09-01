9115b3210dfe2b68731e259bcb79ec82a84165b0
php1301 <19520854@gm.uit.edu.vn>
2020-09-01 22:34:02 +0700

    Auth fixed

--- library/helpers/restriction.jsx
+++ library/helpers/restriction.jsx
@@ -2,17 +2,6 @@ import nextCookie from 'next-cookies';
 import { getCookie, TOKEN_COOKIE, USER_COOKIE } from './session';
 import redirect from './redirect';
 
-function doesHttpOnlyCookieExist(cookiename) {
-  const d = new Date();
-  d.setTime(d.getTime() + (1000));
-  const expires = `expires=${d.toUTCString()}`;
-
-  document.cookie = `${cookiename}=new_value;path=/;${expires}`;
-  if (document.cookie.indexOf(`${cookiename}=`) === -1) {
-    return true;
-  }
-  return false;
-}
 export const isAuthenticated = (ctx) => {
   const token = getCookie(TOKEN_COOKIE, ctx);
   const isLoggedIn = !!token;
@@ -24,12 +13,7 @@ export const isAuthenticated = (ctx) => {
 export const secretPage = (ctx) => {
   // console.log(ctx)
   // ctx từ getInitialProps
-  // Local
-  // const token = getCookie(TOKEN_COOKIE, ctx);
-  // const isLoggedIn = !!token;
-
-  // Prod
-  const token = doesHttpOnlyCookieExist(TOKEN_COOKIE);
+  const token = getCookie(TOKEN_COOKIE, ctx);
   const isLoggedIn = !!token;
   if (!isLoggedIn) {
     // Có thể sử dụng ctx.pathname để lấy prevUrl
@@ -41,8 +25,7 @@ export const secretPage = (ctx) => {
 export const getIsLoggedIn = (ctx) => {
   // console.log(ctx)
   // ctx từ getInitialProps
-  // const token = getCookie(TOKEN_COOKIE, ctx);
-  const token = doesHttpOnlyCookieExist(TOKEN_COOKIE);
+  const token = getCookie(TOKEN_COOKIE, ctx);
   const isLoggedIn = !!token;
   return isLoggedIn;
 };
@@ -64,14 +47,8 @@ export const withChangePasswordSecret = (ctx) => {
   return secret;
 };
 export const withData = (ctx) => {
-  // Trong prod thì chạy hàm set thử cookie ở toke
-  // Nếu ko set được tức là đã có cookie từ BE
-  // Vì BE và FE khác domain nên phải làm cách này
-  // Nếu có domain cùng thì ko cần
-  // const token = getCookie(TOKEN_COOKIE, ctx);
-  const token = doesHttpOnlyCookieExist(TOKEN_COOKIE);
+  const token = getCookie(TOKEN_COOKIE, ctx);
   const isLoggedIn = !!token;
-  // const isLoggedIn = !!token;
   const isUser = getCookie(USER_COOKIE, ctx);
   const userCookie = isUser ? JSON.parse(isUser) : {};
   const user = userCookie || {};
